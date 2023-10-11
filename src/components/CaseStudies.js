9b689a1bae07ae21dd454966ffe4f49cf74c4438
Johannes <kgatle280@gmail.com>
2023-10-12 01:37:17 +0200

    added a slider

--- src/components/CaseStudies.js
+++ src/components/CaseStudies.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import '../assets/styles/CaseStudies.css'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function CaseStudies() {
 
@@ -23,6 +26,35 @@ function CaseStudies() {
         fetchData(); // Call the async function to fetch data from the API endpoint
     }, []); // Empty dependency array ensures useEffect runs once after the initial render
 
+
+    const settings = {
+        dots: true,
+        infinite: true, //enable infinate scrolling
+        speed: 500,
+        slidesToShow: 3, // Number of slides to show at a time
+        slidesToScroll: 3, // Number of slides to scroll
+        autoplay: true, // Enables automatic scrolling
+        autoplaySpeed: 1000, // Delay between slides in milliseconds
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
+
     return (
         <div className='container'>
             <div className='row container heading'>
@@ -30,19 +62,22 @@ function CaseStudies() {
                 <h1 className='col-10 heading-text'>Case Studies</h1>
             </div>
             <div class="row mt-4 mb-4">
-                {cardData.map(card => (
-                    <div class="col-md-4 mb-2">
-                        <div key={card.id} className="card case-study-card">
-                            <img src={card.imageUrl} className="card-img-top" alt={card.title} />
-                            <div className="card-text">
-                                <div className='col-2 line my-auto mb-2'></div>
-                                <h1 className="card-img-title fw-bold">{card.title}</h1>
-                                <p className="case-study-card-text">{card.description}</p>
+                <Slider {...settings}>
+                    {cardData.map(card => (
+                        <div class="col-md-4 mb-2">
+                            <div key={card.id} className="card case-study-card">
+                                <img src={card.imageUrl} className="card-img-top" alt={card.title} />
+                                <div className="card-text">
+                                    <div className='col-2 line my-auto mb-2'></div>
+                                    <h1 className="card-img-title fw-bold">{card.title}</h1>
+                                    <p className="case-study-card-text">{card.description}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </Slider>
             </div>
+
         </div>
     )
 }
